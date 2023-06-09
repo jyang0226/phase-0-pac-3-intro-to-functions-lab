e250e5e11d893e44162d62f43f50b54a95d9cb29
Jaden Falbee <j.falbee04@gmail.com>
2023-06-09 14:50:44 -0400

    changes to index js

---
+++ index.js
@@ -0,0 +1,46 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase();
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  
+  function whisper(string) {
+    // todo
+  }
+  function whisper(string) {
+    return string;
+  }
+  "Hello!".toLowerCase();
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+
+const string = "Hello!"
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+   var noHear = "I can\'t hear you!";
+   var yesHear = "YES INDEED!";
+   var dinner = "I would love to!";
+   if (string.toLowerCase(string) === string) {
+    return noHear;
+   }
+   else if (string.toUpperCase(string) === string) {
+    return yesHear;
+   }
+   else if ("Let\'s have dinner together!" === string) {
+    return dinner;
+   }
+}
